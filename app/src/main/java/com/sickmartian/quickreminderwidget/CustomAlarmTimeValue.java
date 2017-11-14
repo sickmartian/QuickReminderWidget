23e926867f5d735b9f2c58f9e29e3e593132e676
sickmartian <sickmartian@gmail.com>
2017-11-13 23:40:07 -0300

    Adds more options into the future that make more sense with snooze

--- app/src/main/java/com/sickmartian/quickreminderwidget/CustomAlarmTimeValue.java
+++ app/src/main/java/com/sickmartian/quickreminderwidget/CustomAlarmTimeValue.java
@@ -12,7 +12,8 @@ public class CustomAlarmTimeValue {
             1, 5, 10, 15,
             20, 25, 30, 35,
             40, 45, 60, 90,
-            120, 180, 240
+            120, 180, 240, 360,
+            720, 1440, 2880
     };
     public static int[] customValueCorrespondingNames = new int[]{
             R.string.custom_values_disabled_label,
@@ -30,7 +31,11 @@ public class CustomAlarmTimeValue {
             R.string.custom_values_90_label,
             R.string.custom_values_120_label,
             R.string.custom_values_180_label,
-            R.string.custom_values_240_label
+            R.string.custom_values_240_label,
+            R.string.custom_values_360_label,
+            R.string.custom_values_720_label,
+            R.string.custom_values_1440_label,
+            R.string.custom_values_2880_label
     };
     public static int[] customValueCorrespondingShort = new int[]{
             R.string.custom_values_disabled_label_short,
@@ -48,7 +53,11 @@ public class CustomAlarmTimeValue {
             R.string.custom_values_90_label_short,
             R.string.custom_values_120_label_short,
             R.string.custom_values_180_label_short,
-            R.string.custom_values_240_label_short
+            R.string.custom_values_240_label_short,
+            R.string.custom_values_360_label_short,
+            R.string.custom_values_720_label_short,
+            R.string.custom_values_1440_label_short,
+            R.string.custom_values_2880_label_short
     };
 
     public static String getCustomValueShortLabel(int minutes) {
